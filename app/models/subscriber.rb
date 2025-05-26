e6f2c2f73d0d6340d8d4f22c84266fd79d35db14
graydon-whitespark <graydon@whitespark.ca>
2025-05-26 14:48:15 -0600

    add unsubscribing to emails

--- app/models/subscriber.rb
+++ app/models/subscriber.rb
@@ -1,3 +1,7 @@
 class Subscriber < ApplicationRecord
   belongs_to :product
+
+  # generates a signed token containing the purpose, a timestamp, a cryptographic signature, and the subscribers id
+  # the token is not stored
+  generates_token_for :unsubscribe
 end
